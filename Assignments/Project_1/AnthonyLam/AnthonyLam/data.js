aafe530db3d52f5bf704e5ca6738d305491dfc34
ANTHONY <anthony.lam02@sjsu.edu>
2026-02-18 20:14:10 -0800

    Update data.js

--- Assignments/Project_1/AnthonyLam/AnthonyLam/data.js
+++ Assignments/Project_1/AnthonyLam/AnthonyLam/data.js
@@ -1,1 +1,43 @@
+const DAYS = [
+  {
+    day: "MON",
+    social: 3,
+    entertainment: 2,
+    productivity: 1,
+    creativity: 1,
+    other: 1
+  },
+  {
+    day: "TUE",
+    social: 4,
+    entertainment: 1,
+    productivity: 2,
+    creativity: 1,
+    other: 1
+  },
+  {
+    day: "WED",
+    social: 2,
+    entertainment: 3,
+    productivity: 2,
+    creativity: 1,
+    other: 1
+  },
+  {
+    day: "THU",
+    social: 5,
+    entertainment: 1,
+    productivity: 1,
+    creativity: 1,
+    other: 1
+  },
+  {
+    day: "FRI",
+    social: 3,
+    entertainment: 4,
+    productivity: 1,
+    creativity: 2,
+    other: 1
+  }
+];
 
